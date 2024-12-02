13b70df6982f2b25cc100bb26c3c1f1a3de8d566
Ryan JG <ryangregoire286@gmail.com>
2024-12-02 14:34:27 -0500

    Added Try Catch Exception Handling

--- src/Main.java
+++ src/Main.java
@@ -12,34 +12,38 @@ public class Main {
     public static String Name2;
     public static String RandomName;
 
-    public Main(String name1, String name2) {
+    public Main(final String name1, final String name2) {
         super();
         Name = name1;
         Name2 = name2;
     }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Main m = null;
+        try {
+            Scanner scan = new Scanner(System.in);
+            Main m = null;
 
-        System.out.println("Enter Name 1: ");
-        Name = scan.nextLine();
-        System.out.println("Enter Name 2: ");
-        Name2 = scan.nextLine();
+            List<String> names = new ArrayList<String>();
+            names.add(Name);
+            names.add(Name2);
 
-        Main main = new Main(Name, Name2);
-        System.out.println("Name 1: " + Main.Name);
-        System.out.println("Name 2: " + Main.Name2);
+            System.out.println("Enter Name 1: ");
+            Name = scan.nextLine();
+            System.out.println("Enter Name 2: ");
+            Name2 = scan.nextLine();
 
-        List<String> names = new ArrayList<String>();
-        names.add(Name);
-        names.add(Name2);
-        int randomNamePicker = r.nextInt(names.size());
-        String n = names.get(randomNamePicker);
+            Main main = new Main(Name, Name2);
+            System.out.println("Name 1: " + Main.Name);
+            System.out.println("Name 2: " + Main.Name2);
 
-        System.out.println("Random Name Picked is: " + n);
+            int randomNamePicker = r.nextInt(names.size());
+            String n = names.get(randomNamePicker);
+
+            System.out.println("Random Name Picked is: " + n);
+        } catch(Exception ce) {
+            System.out.println(ce.getMessage());
+        }
     }
-
 
 
 }
