48c4812e92a1952c8fb671f1c78f479b49c5ff24
Ryan JG <ryangregoire286@gmail.com>
2024-12-02 14:21:10 -0500

    Added Code Today for Application

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static Random r = new Random();
+
+
+    public static String Name;
+    public static String Name2;
+    public static String RandomName;
+
+    public Main(String name1, String name2) {
+        super();
+        Name = name1;
+        Name2 = name2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Main m = null;
+
+        System.out.println("Enter Name 1: ");
+        Name = scan.nextLine();
+        System.out.println("Enter Name 2: ");
+        Name2 = scan.nextLine();
+
+        Main main = new Main(Name, Name2);
+        System.out.println("Name 1: " + Main.Name);
+        System.out.println("Name 2: " + Main.Name2);
+
+        List<String> names = new ArrayList<String>();
+        names.add(Name);
+        names.add(Name2);
+        int randomNamePicker = r.nextInt(names.size());
+        String n = names.get(randomNamePicker);
+
+        System.out.println("Random Name Picked is: " + n);
+    }
+
+
+
+}
+
